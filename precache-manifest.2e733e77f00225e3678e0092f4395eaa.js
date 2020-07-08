2baaecf35da5daf582e4485fec51be70c7741fb7
PzOng <pangszee.ong@gmail.com>
2020-07-08 22:11:12 +0930

    Updates

---
+++ precache-manifest.2e733e77f00225e3678e0092f4395eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462ba0fa09148486acd53b6ed9142d9e",
+    "url": "/ReactEmpDir/index.html"
+  },
+  {
+    "revision": "164c8d369d6099fa4512",
+    "url": "/ReactEmpDir/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "42e4b39531eb474d0d47",
+    "url": "/ReactEmpDir/static/js/2.3074dd66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactEmpDir/static/js/2.3074dd66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164c8d369d6099fa4512",
+    "url": "/ReactEmpDir/static/js/main.01787eed.chunk.js"
+  },
+  {
+    "revision": "4111984b0559afbf2d72",
+    "url": "/ReactEmpDir/static/js/runtime-main.b0df9253.js"
+  }
+]);
